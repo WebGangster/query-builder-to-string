bb9983d74fb2b93dc5ec747e77ea24fb8128d31b
Anatoly Bubenkov <bubenkoff@gmail.com>
2019-12-04 05:48:49 +0100

    support contains and not contains operators

--- index.js
+++ index.js
@@ -30,80 +30,104 @@ function convertRule(input) {
     var mongoOperatorsFns = {
 
         equal: function(v) {
-
-			if(typeof v[1] == 'string'){
-				v[1] = "'" + v[1] + "'";
-			}
-            
-			var getValue = parseFloat(v[1]);
-
-            if(!isNaN(getValue)){
-                if(typeof getValue == 'number'){
-                    v[1] = getValue;
-                }
+            if(typeof v[1] == 'string'){
+              v[1] = "'" + v[1] + "'";
             }
-			
-			return 'this.'+ v[0] + " == " + v[1]; 
+
+            var getValue = parseFloat(v[1]);
+
+                  if(!isNaN(getValue)){
+                      if(typeof getValue == 'number'){
+                          v[1] = getValue;
+                      }
+                  }
+
+            return 'this.'+ v[0] + " == " + v[1];
         },
         not_equal: function(v) {
 
             if(typeof v[1] == 'string'){
-				v[1] = "'" + v[1] + "'";
-			}
-			
-			var getValue = parseFloat(v[1]);
+              v[1] = "'" + v[1] + "'";
+            }
+
+            var getValue = parseFloat(v[1]);
+
+                  if(!isNaN(getValue)){
+                      if(typeof getValue == 'number'){
+                          v[1] = getValue;
+                      }
+                  }
+
+            return 'this.'+ v[0] + " != " + v[1];
+        },
+        less: function(v) {
+            var getValue = parseFloat(v[1]);
+
+            if (typeof getValue == 'number') {
+                v[1] = parseFloat(getValue);
+            }
+
+            return 'this.'+ v[0] + " < " + v[1];
+        },
+        less_or_equal: function(v) {
+            var getValue = parseFloat(v[1]);
+
+            if (typeof getValue == 'number') {
+                v[1] = parseFloat(getValue);
+            }
+
+            return 'this.'+ v[0] + " <= " + v[1];
+        },
+        greater: function(v) {
+
+            var getValue = parseFloat(v[1]);
+
+            if (typeof getValue == 'number') {
+                v[1] = parseFloat(getValue);
+            }
+
+            return 'this.'+ v[0] + " > " + v[1];
+        },
+        greater_or_equal: function(v) {
+
+            var getValue = parseFloat(v[1]);
+
+            if (typeof getValue == 'number') {
+                v[1] = parseFloat(getValue);
+            }
+
+            return 'this.'+ v[0] + " >= " + v[1];
+        },
+        contains: function(v) {
+            if(typeof v[1] == 'string'){
+              v[1] = "'" + v[1] + "'";
+            }
+
+            var getValue = parseFloat(v[1]);
 
             if(!isNaN(getValue)){
                 if(typeof getValue == 'number'){
                     v[1] = getValue;
                 }
             }
-			
-			return 'this.'+ v[0] + " != " + v[1]; 
+
+            return 'this.'+ v[0] + ".indexOf(" + v[1] + ") > -1";
         },
-        less: function(v) {
-		
-	    var getValue = parseFloat(v[1]);
-		
-            if (typeof getValue == 'number') {
-                v[1] = parseFloat(getValue);
+        not_contains: function(v) {
+
+            if(typeof v[1] == 'string'){
+              v[1] = "'" + v[1] + "'";
             }
-			
-			return 'this.'+ v[0] + " < " + v[1];           
-			
-        },
-        less_or_equal: function(v) {
 
             var getValue = parseFloat(v[1]);
-		
-            if (typeof getValue == 'number') {
-                v[1] = parseFloat(getValue);
+
+            if(!isNaN(getValue)){
+                if(typeof getValue == 'number'){
+                    v[1] = getValue;
+                }
             }
-			
-			return 'this.'+ v[0] + " <= " + v[1]; 
 
-        },
-        greater: function(v) {
-
-            var getValue = parseFloat(v[1]);
-		
-            if (typeof getValue == 'number') {
-                v[1] = parseFloat(getValue);
-            }
-			
-			return 'this.'+ v[0] + " > " + v[1]; 
-
-        },
-        greater_or_equal: function(v) {
-
-            var getValue = parseFloat(v[1]);
-		
-            if (typeof getValue == 'number') {
-                v[1] = parseFloat(getValue);
-            }
-			
-			return 'this.'+ v[0] + " >= " + v[1]; 
-
+            return 'this.'+ v[0] + ".indexOf(" + v[1] + ") === -1";
         }
     }
 
@@ -136,14 +160,14 @@ function convertRule(input) {
                 parts.push(parse(rule));
             } else {
                 var expFunction = mongoOperatorsFns[rule.operator];
-				
-				var ruleExpression = {};
+
+        var ruleExpression = {};
 
                 if (expFunction === undefined) {
                     throw new Error('Unknown rule operation for operator ' + rule.operator);
                 }
 
-				ruleExpression = expFunction.call(self, [rule.id, rule.value]);
+        ruleExpression = expFunction.call(self, [rule.id, rule.value]);
 
                 parts.push(ruleExpression);
             }
@@ -151,18 +175,18 @@ function convertRule(input) {
 
         var groupExpression = {};
 
-		var output = '';
-		
-		for(var i=0; i < parts.length; i++) {
-			if(typeof parts[i+1] != 'undefined') {
-				output += parts[i] + (group.condition.toLowerCase() == 'and' ? ' && ' : ' || ');
-			} else {
-				output += parts[i];
-			}
-		}	
-		
-		output = '(' + output + ')';
-		
+    var output = '';
+
+    for(var i=0; i < parts.length; i++) {
+      if(typeof parts[i+1] != 'undefined') {
+        output += parts[i] + (group.condition.toLowerCase() == 'and' ? ' && ' : ' || ');
+      } else {
+        output += parts[i];
+      }
+    }
+
+    output = '(' + output + ')';
+
         return (output);
 
     })(input);
